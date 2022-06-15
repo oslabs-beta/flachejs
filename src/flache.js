2b4440852ffa780616c01f1a98bd2850d8a368b5
twojaytech <twojaytech@gmail.com>
2022-06-15 16:59:36 -0400

    initial commit flache

---
+++ src/flache.js
@@ -0,0 +1,20 @@
+const localforage = require("../node_modules/localforage");
+
+localforage.config({
+  name: 'clientCache',
+  storeName: 'http',
+  description: 'A cache for client HTTP requests',
+  version: 1.0
+})
+
+const store = localforage.createInstance({
+  name: 'httpCache'
+})
+
+store.setItem('test', { message: 'this is a test to see if this even works' });
+
+store.getItem('test').then(res => console.log(res))
+
+// async function Flache() {}
+
+// module.exports = {Flache}
