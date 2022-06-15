f15285bd003a169c89d714d29e291d203515bb70
= <=>
2022-06-15 19:20:23 -0400

    added webpack config for module loading

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    index: './src/flache.js'
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:'index.html'
+    })
+  ],
+
+  devServer: {
+    compress: true,
+    port: 8080,
+  }
+}
