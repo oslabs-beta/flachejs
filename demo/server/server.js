d2e55252f6ab192f5da1e0152d2ea306a43dcf00
eyeandko <izuberi776@gmail.com>
2022-06-24 03:56:23 +0300

    installing socket.io and setting up server WS connection

--- demo/server/server.js
+++ demo/server/server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const PORT = 3000;
 require('dotenv').config();
 
+const connection = mongoose.connection;
+
 // Database Connection
 mongoose.connect(
     'mongodb+srv://twojay:PtTS83QW2JVJsB7j@cluster0.ueoejsn.mongodb.net/?retryWrites=true&w=majority',
@@ -12,6 +14,18 @@ mongoose.connect(
   )
 mongoose.connection.once("open", () => {
   console.log("Connected to Database");
+
+  console.log("Setting change streams");
+  const bookChangeStream = connection.collection("books").watch();
+
+  bookChangeStream.on("change", (change) => {
+    switch (change.operationType) {
+      case "insert":
+        const book = change.fullDocument;
+        console.log("New book added: ", book);
+        io.of("/api/socket").emit("newBook", book);
+        break;
+      }})
 });
 
 // Express App Requirements
@@ -34,7 +48,23 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Listening on Port: ${PORT}`);
+// Web Socket connection
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {cors: '*'});
+
+io.of("/api/socket").on("connection", (socket) => {
+  console.log("socket.io: User connected: ", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("socket.io: User disconnected: ", socket.id);
+  });
 });
+
+// Start the server
+server.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
+
+// Start Server
+/* app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`);
+}); */
+
