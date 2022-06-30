153efc052dcd734d9664ee7e21be693dfa74b95c
twojaytech <twojaytech@gmail.com>
2022-06-30 19:21:10 -0400

    basic working devtool

--- extension/src/devtools/devtools.js
+++ extension/src/devtools/devtools.js
@@ -4,8 +4,8 @@ chrome.devtools.panels.create(
   // path to an icon
   null, 
   // html page for injecting into the tab's content
-  'panel.html',
-  // './src/panel.html', // for some reason it needs entire path from src onwards
+  // 'panel.html',
+  './src/panel.html', // for some reason it needs entire path from src onwards
   // some callback function
   // null 
 );
